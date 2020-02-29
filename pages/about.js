60477e589ccd34e34b43131d02408e50ce6a4f0c
Sven <sven.herzing@gmail.com>
2020-02-29 13:46:39 +0400

    empty index and about page

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+class About extends React.Component {
+
+    render() {
+        return (
+            <h1> About class</h1>
+        )
+    }
+}
+
+
+export default About
