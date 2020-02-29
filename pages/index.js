5bebaf98116d44a19aee8a08984af647016e12a3
Sven <sven.herzing@gmail.com>
2020-02-29 23:58:25 +0400

    react lifecycle functios and basic state

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,37 @@ import BaseLayout from '../components/layouts/BaseLayout'
 
 class Index extends React.Component {
     
-    render() {   
+    constructor() {
+        super()
+        this.state = {
+            title: 'I am index page'
+        }
+        console.log('constructor')
+    }
+
+    componentDidMount() {
+        console.log('componentDidMount')
+    }
+
+    componentDidUPdate() {
+        console.log('componentDidUpdate')
+    }
+
+    componentWillUnmount() {
+        console.log('componentWillUnmount')
+    }
+
+    updateTitle() {
+        this.setState({title : 'I am Updated index page'})
+    }
+
+    render() { 
+        console.log('render')  
         return (
             <BaseLayout>
-                <h1> Index class</h1>
+                <h1 className='fromPage'>Index class</h1>
+                <h2> {this.state.title} </h2>
+                <button onClick={ () => this.updateTitle()}>Change title</button>
             </BaseLayout>
         )
     }
